0e77ee2d47014a106e0c7f0b00005a607f585689
Frederick Martian <fredmartian@gmail.com>
2011-10-23 10:21:14 +0000

    Add more details to the GridEditor dialog

--- src/libomv/Gui/dialogs/GridEditor.java
+++ src/libomv/Gui/dialogs/GridEditor.java
@@ -29,6 +29,12 @@ import javax.swing.event.ListSelectionListener;
 
 import libomv.GridClient;
 import libomv.GridClient.GridInfo;
+import libomv.utils.Helpers;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
 
 public class GridEditor extends JDialog
 {
@@ -52,6 +58,12 @@ public class GridEditor extends JDialog
 	private JTextField jTxtSupportUrl;
 	private JTextField jTxtRegisterUrl;
 	private JTextField jTxtPasswordUrl;
+	private JPanel jPaneGridBtnPanel;
+	private JButton jBtnClearGridInfo;
+	private JPanel jBtnPanel;
+	private JButton jBtnApply;
+	private JButton jBtnAdd;
+	private JButton jBtnRemove;
 
 	public GridEditor(GridClient client, JFrame parent, String title, boolean modal)
 	{
@@ -65,7 +77,7 @@ public class GridEditor extends JDialog
 		
 		// Do not allow resizing
 		setResizable(false);
-		setSize(new Dimension(640, 464));
+		setSize(new Dimension(663, 464));
 		setLocationByPlatform(true);
 		
 		getContentPane().add(getJSpGridNames(), BorderLayout.WEST);
@@ -73,11 +85,13 @@ public class GridEditor extends JDialog
 		JPanel jEditPanel = new JPanel();
 		getContentPane().add(jEditPanel, BorderLayout.CENTER);
 
+		getContentPane().add(getButtonPanel(), BorderLayout.SOUTH);
+
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{1, 2, 3, 1, 0};
+		gridBagLayout.columnWidths = new int[]{1, 2, 3, 0, 1, 0};
 		gridBagLayout.rowHeights = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		jEditPanel.setLayout(gridBagLayout);
 		
 		JLabel jLblName = new JLabel("Name");
@@ -124,12 +138,13 @@ public class GridEditor extends JDialog
 		gbConstraints.gridx = 2;
 		gbConstraints.gridy = 3;
 		jEditPanel.add(getJTxtLoginUrl(), gbConstraints);
-		
+
 		gbConstraints = new GridBagConstraints();
 		gbConstraints.insets = new Insets(0, 0, 5, 5);
+		gbConstraints.fill = GridBagConstraints.BOTH;
 		gbConstraints.gridx = 2;
 		gbConstraints.gridy = 4;
-		jEditPanel.add(getJBtnSetup(), gbConstraints);
+		jEditPanel.add(getGridBtnPanel(), gbConstraints);
 		
 		JLabel jLblStartUrl = new JLabel("Login Page:");
 		gbConstraints = new GridBagConstraints();
@@ -221,19 +236,7 @@ public class GridEditor extends JDialog
 		gbConstraints.gridy = 10;
 		jEditPanel.add(getJTxtPasswordUrl(), gbConstraints);
 		
-		gbConstraints = new GridBagConstraints();
-		gbConstraints.insets = new Insets(5, 0, 5, 5);
-		gbConstraints.gridx = 1;
-		gbConstraints.gridy = 12;
-		jEditPanel.add(getJBtnOk(), gbConstraints);
-		
 		getRootPane().setDefaultButton(getJBtnOk());
-		
-		gbConstraints = new GridBagConstraints();
-		gbConstraints.insets = new Insets(5, 0, 5, 5);
-		gbConstraints.gridx = 2;
-		gbConstraints.gridy = 12;
-		jEditPanel.add(getJBtnCancel(), gbConstraints);
 		updateGridProperties(_Client.getDefaultGrid(), false);
 	}
 	
@@ -256,6 +259,76 @@ public class GridEditor extends JDialog
 			});
 		}
 		return jBtnSetup;
+	}
+
+	private JButton getJBtnClearGridInfo() {
+		if (jBtnClearGridInfo == null) {
+			jBtnClearGridInfo = new JButton("Clear Grid Info");
+			jBtnClearGridInfo.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent arg0)
+				{
+					updateGridProperties(null, false);
+				}
+			});
+		}
+		return jBtnClearGridInfo;
+	}
+
+	private JButton getJBtnApply() {
+		if (jBtnApply == null) {
+			jBtnApply = new JButton("Apply");
+			jBtnApply.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent arg0)
+				{
+					updateGridProperties((GridInfo)(getJLsGridNames().getSelectedValue()), true);
+				}
+			});
+		}
+		return jBtnApply;
+	}
+
+	private JPanel getGridBtnPanel() {
+		if (jPaneGridBtnPanel == null) {
+			jPaneGridBtnPanel = new JPanel();
+			jPaneGridBtnPanel.add(getJBtnSetup());
+			jPaneGridBtnPanel.add(getJBtnClearGridInfo());
+			jPaneGridBtnPanel.add(getJBtnApply());
+		}
+		return jPaneGridBtnPanel;
+	}
+
+	private JButton getJBtnAdd() {
+		if (jBtnAdd == null) {
+			jBtnAdd = new JButton("Add Grid");
+			jBtnAdd.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent arg0)
+				{
+					dispose();
+				}
+			});
+		}
+		return jBtnAdd;
+	}
+	
+	private JButton getBtnNewButton() {
+		if (jBtnRemove == null) {
+			jBtnRemove = new JButton("Remove Grid");
+			jBtnRemove.addActionListener(new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent arg0)
+				{
+					dispose();
+				}
+			});
+		}
+		return jBtnRemove;
 	}
 	
 	private JButton getJBtnOk()
@@ -298,6 +371,63 @@ public class GridEditor extends JDialog
 			});
 		}
 		return jBtnCancel;
+	}
+
+	private JPanel getButtonPanel() {
+		if (jBtnPanel == null) {
+			jBtnPanel = new JPanel();
+			jBtnPanel.setLayout(new FormLayout(new ColumnSpec[] {
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,
+					FormFactory.RELATED_GAP_COLSPEC,
+					FormFactory.DEFAULT_COLSPEC,},
+				new RowSpec[] {
+					FormFactory.RELATED_GAP_ROWSPEC,
+					FormFactory.DEFAULT_ROWSPEC,}));
+			jBtnPanel.add(getJBtnAdd(), "2, 2");
+			jBtnPanel.add(getBtnNewButton(), "6, 2, default, center");
+			jBtnPanel.add(getJBtnCancel(), "20, 2");
+			jBtnPanel.add(getJBtnOk(), "24, 2");
+		}
+		return jBtnPanel;
 	}
 
 	private JTextField getJTxtName()
@@ -691,9 +821,7 @@ public class GridEditor extends JDialog
 				@Override
 				public void valueChanged(ListSelectionEvent e)
 				{
-					updateGridProperties(lastSelection, true);
-					lastSelection = (GridInfo)((JList)e.getSource()).getSelectedValue();
-					updateGridProperties(lastSelection, false);				
+					updateGridProperties((GridInfo)((JList)e.getSource()).getSelectedValue(), false);				
 				}
 			});
 		}
@@ -726,15 +854,15 @@ public class GridEditor extends JDialog
 		}
 		else
 		{
-			getJTxtName().setText(grid.gridname);
-			getJTxtNick().setText(grid.gridnick);
-			getJTxtStartUrl().setText(grid.loginpage);
-			getJTxtLoginUrl().setText(grid.loginuri);
-			getJTxtHelperUrl().setText(grid.helperuri);
-			getJTxtWebsiteUrl().setText(grid.website);
-			getJTxtSupportUrl().setText(grid.support);
-			getJTxtRegisterUrl().setText(grid.register);
-			getJTxtPasswordUrl().setText(grid.passworduri);
+			getJTxtName().setText(grid == null ? Helpers.EmptyString : grid.gridname);
+			getJTxtNick().setText(grid == null ? Helpers.EmptyString : grid.gridnick);
+			getJTxtStartUrl().setText(grid == null ? Helpers.EmptyString : grid.loginpage);
+			getJTxtLoginUrl().setText(grid == null ? Helpers.EmptyString : grid.loginuri);
+			getJTxtHelperUrl().setText(grid == null ? Helpers.EmptyString : grid.helperuri);
+			getJTxtWebsiteUrl().setText(grid == null ? Helpers.EmptyString : grid.website);
+			getJTxtSupportUrl().setText(grid == null ? Helpers.EmptyString : grid.support);
+			getJTxtRegisterUrl().setText(grid == null ? Helpers.EmptyString : grid.register);
+			getJTxtPasswordUrl().setText(grid == null ? Helpers.EmptyString : grid.passworduri);
 		}
 	}
 }
