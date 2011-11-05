edfd691b3deb505453fe5985907bf74c8ca1f904
Frederick Martian <fredmartian@gmail.com>
2011-11-05 09:47:45 +0000

    Add copyright header and fix some compile errors

--- src/libomv/assets/AssetTexture.java
+++ src/libomv/assets/AssetTexture.java
@@ -1,9 +1,37 @@
+/**
+ * Copyright (c) 2009, openmetaverse.org
+ * Copyright (c) 2009-2011, Frederick Martian
+ * All rights reserved.
+ *
+ * - Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * - Neither the name of the openmetaverse.org nor the names
+ *   of its contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package libomv.assets;
 
 import libomv.imaging.ManagedImage;
 import libomv.imaging.J2KWrap;
 import libomv.imaging.J2KWrap.J2KLayerInfo;
 import libomv.types.UUID;
+import libomv.utils.Logger;
+import libomv.utils.Logger.LogLevel;
 
 public class AssetTexture extends AssetItem
 {
@@ -66,7 +94,7 @@ public class AssetTexture extends AssetItem
 	@Override
 	public void Encode()
 	{
-		AssetData = J2KWrap.Encode(Image);
+		AssetData = J2KWrap.encode(Image);
 	}
 
 	/**
@@ -80,7 +108,15 @@ public class AssetTexture extends AssetItem
 	{
 		Components = 0;
 
-		Image = J2KWrap.DecodeToImage(AssetData);
+		try
+		{
+			Image = J2KWrap.decode(AssetData);
+		}
+		catch (Exception ex)
+		{
+			Logger.Log("Error decoding asset texture data", LogLevel.Error, ex);
+		}
+
 		if (Image != null)
 		{
 			if ((Image.Channels & ManagedImage.ImageChannels.Color) != 0)
@@ -98,14 +134,13 @@ public class AssetTexture extends AssetItem
 	}
 
 	/**
-	 * Decodes the begin and end byte positions for each quality layer in the
-	 * image
+	 * Decodes the begin and end byte positions for each quality layer in the image
 	 * 
 	 * @return
 	 */
 	public boolean DecodeLayerBoundaries()
 	{
-		Components = J2KWrap.DecodeLayerBoundaries(AssetData, LayerInfo);
+		Components = J2KWrap.decodeLayerBoundaries(AssetData, LayerInfo);
 		return (Components > 0);
 	}
 
