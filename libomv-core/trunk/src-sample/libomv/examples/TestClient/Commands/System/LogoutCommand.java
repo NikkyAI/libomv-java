944aa6b3a462fcf8b534624f3b4305f43dd690e7
Frederick Martian <fredmartian@gmail.com>
2012-04-03 19:19:32 +0000

    Add a logout command and a dummy login command

---
+++ libomv-core/trunk/src-sample/libomv/examples/TestClient/Commands/System/LogoutCommand.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2009, openmetaverse.org
+ * Copyright (c) 2012, Frederick Martian
+ * All rights reserved.
+ *
+ * - Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * - Neither the name of the openmetaverse.org nor the names
+ *   of its contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package libomv.examples.TestClient.Commands.System;
+
+import libomv.examples.TestClient.Command;
+import libomv.examples.TestClient.TestClient;
+import libomv.types.UUID;
+import libomv.utils.Logger;
+import libomv.utils.Logger.LogLevel;
+
+public class LogoutCommand extends Command
+{
+    public LogoutCommand(TestClient testClient)
+    {
+        Name = "logout";
+        Description = "Log this avatar out";
+        Category = CommandCategory.TestClient;
+    }
+
+    @Override
+	public String Execute(String[] args, UUID fromAgentID)
+    {
+        String name = Client.toString();
+	    try
+		{
+			Client._ClientManager.Logout(Client);
+		}
+		catch (Exception ex)
+		{
+			Logger.Log("Error logging out", LogLevel.Error, Client, ex);
+		}
+        return "Logged " + name + " out";
+    }
+}
