d3387d0cdefb3d47ec0a2a2d465d93aa373ad701
Frederick Martian <fredmartian@gmail.com>
2012-06-03 18:31:31 +0000

    Needed ResetControlFlags to be public for movement

--- libomv-core/trunk/src/libomv/AgentManager.java
+++ libomv-core/trunk/src/libomv/AgentManager.java
@@ -6721,7 +6721,7 @@ public class AgentManager implements PacketCallback, CapsCallback
 			}
 		}
 
-		private void ResetControlFlags()
+		public void ResetControlFlags()
 		{
 			// Reset all of the flags except for persistent settings like
 			// away, fly, mouselook, and crouching
