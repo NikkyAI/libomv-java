d8d36381504545122b88149cf80e1e1c50d7e314
Frederick Martian <fredmartian@gmail.com>
2012-09-20 07:43:55 +0000

    Modify the copyright clause and add it to files that are missing it

--- libomv-gui/trunk/src/libomv/Gui/components/OnlinePanel.java
+++ libomv-gui/trunk/src/libomv/Gui/components/OnlinePanel.java
@@ -2,13 +2,16 @@
  * Copyright (c) 2010-2011, Frederick Martian
  * All rights reserved.
  *
- * - Redistribution and use in source and binary forms, with or without
- *   modification, are permitted provided that the following conditions are met:
- *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
  * - Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * - Neither the name of the openmetaverse.org nor the names
- *   of its contributors may be used to endorse or promote products derived from
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - Neither the name of the libomv-java project nor the names of its
+ *   contributors may be used to endorse or promote products derived from
  *   this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -187,6 +190,17 @@ public class OnlinePanel extends JPanel implements ActionListener
 		public boolean callback(BalanceCallbackArgs params)
 		{
 			getJAmount().setText(String.format("%s %s", _Client.getGrid(null).currencySym, params.getBalance()));
+			if (!params.getFirst() && params.getDelta() > 50)
+			{
+				if (params.getDelta() < 0)
+				{
+					
+				}
+				else
+				{
+					
+				}
+			}
 			return false;
 		}
 		
