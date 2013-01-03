35c4775eb9b3694957ea0512db610e979f3d6253
Frederick Martian <fredmartian@gmail.com>
2013-01-03 12:49:16 +0000

    Add emptytrash command

---
+++ libomv-core/trunk/src-sample/libomv/examples/TestClient/Commands/Inventory/EmptyTrashCommand.java
@@ -0,0 +1,24 @@
+package libomv.examples.TestClient.Commands.Inventory;
+
+import libomv.examples.TestClient.Command;
+import libomv.examples.TestClient.TestClient;
+import libomv.inventory.InventoryException;
+import libomv.types.UUID;
+
+/// TestClient command to download and display a notecard asset
+public class EmptyTrashCommand extends Command
+{
+    public EmptyTrashCommand(TestClient testClient)
+    {
+        Name = "emptytrash";
+        Description = "Empty inventory Trash folder";
+        Category = CommandCategory.Inventory;
+    }
+
+    @Override
+    public String execute(String[] args, UUID fromAgentID) throws InventoryException, Exception
+    {
+        Client.Inventory.EmptyTrash();
+        return "Trash Emptied";
+    }
+}
