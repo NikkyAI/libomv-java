40490b9cbb263760b2a226bca61d8da37d71685e
Frederick Martian <fredmartian@gmail.com>
2013-12-25 22:45:46 +0000

    Added extended terrain/cloud/wind/... compressed patch types for larger regions.
    by Robert Adams

--- libomv-core/trunk/src/libomv/TerrainManager.java
+++ libomv-core/trunk/src/libomv/TerrainManager.java
@@ -46,7 +46,15 @@ public class TerrainManager implements PacketCallback
 {
 	public enum LayerType
 	{
-		Land(0x4C), Water(0x57), Wind(0x37), Cloud(0x38);
+		Land            (0x4C), // 'L'
+        LandExtended    (0x4D), // 'M'
+        Water           (0x57), // 'W'
+        WaterExtended   (0x58), // 'X'
+        Wind            (0x37), // '7'
+        WindExtended    (0x39), // '9'
+        Cloud           (0x38), // '8'
+        CloudExtended   (0x3A); // ':'
+
 
 		public static LayerType setValue(int value)
 		{
