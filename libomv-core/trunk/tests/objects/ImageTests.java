8261396cbd44ff04549881e3c09488a69c46b14d
Frederick Martian <fredmartian@gmail.com>
2017-01-28 15:03:11 +0000

    Make ImageTest use new image in resources

--- libomv-core/trunk/tests/objects/ImageTests.java
+++ libomv-core/trunk/tests/objects/ImageTests.java
@@ -1,7 +1,6 @@
 package objects;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 import junit.framework.TestCase;
 import libomv.imaging.J2KImage;
@@ -10,8 +9,7 @@ public class ImageTests extends TestCase
 {
 	public void testJ2KImage() throws Exception
 	{
-		File file = new File("D:\\Documents\\My Documents\\Downloads\\Bretagne1.j2k");
-		FileInputStream is = new FileInputStream(file);
+		InputStream is = getClass().getResourceAsStream("/res/relax.jp2");
         J2KImage image = new J2KImage(is);
         assertTrue("Loading of image failed", image != null);
         assertTrue("Loading of image failed", image.Channels == 3);
